ae2a6f650a638065c56276155e7257e4584cb350
leanhtoan <leanhtoan@192.168.2.75>
2013-10-11 09:48:20 +0700

    test 222

---
+++ class2.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of class2
+ *
+ * @author  Steve Do <do.hung@mulodo.com>
+ */
+class class2 {
+    //put your code here
+}
+
+?>
